54a8ae1cb76cbe918327f165c4e9fc017a97f3f9
Nickolay68 <p.nick21vek@gmail.com>
2024-08-10 22:57:38 +0300

    add code base

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ SCREEN_HEIGHT = 600
 screen =  pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/1234.jpg")
+icon = pygame.image.load("img/мишень.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
@@ -24,7 +24,7 @@ while running:
     pass
     screen.fill(color)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
