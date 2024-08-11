d19221c30c65692329d0cd95eac4c26b4216f169
Nickolay68 <p.nick21vek@gmail.com>
2024-08-11 14:50:56 +0300

    add new const

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/мишень.jpg")
+pygame.display.set_icon(icon)
